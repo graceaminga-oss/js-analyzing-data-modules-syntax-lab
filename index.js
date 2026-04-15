5b214b0c14cc36e0eb8890091eab0be9a59aa341
grace aminga <grace.aminga@student.moringaschool.com>
2026-04-15 13:28:41 +0300

    added code

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+// import datejs
+require('datejs');
 
+function combineUsers(...args) {
+  // Initialize return object
+  const combinedObject = {
+    users: []
+  };
 
+  // Loop through args and merge arrays
+  args.forEach(array => {
+    combinedObject.users = [...combinedObject.users, ...array];
+  });
+
+  // Get today's date
+  const today = new Date();
+  combinedObject.merge_date = today.toString('M/d/yyyy');
+
+  // Return object
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
